a905197533ddb5ff7777b1f81cd73c93fb23025c
ali-anas <infoalmora786@gmail.com>
2020-08-16 20:39:01 +0530

    saved tokens and cookies and managed login-logout display conditions on both ssr and client side

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
-import React from 'react'; 
+import React from 'react';
+import auth0 from '../services/auth0';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/main.scss";
 import { Component } from "react";
+
 
 class App extends React.Component {
 
     static async getInitialProps({Component, ctx}) {
         let pageProps = {};
+        const isAuthenticated = process.browser ? auth0.clientAuth() : auth0.serverAuth(ctx.req);
+
+        console.log(isAuthenticated);
 
         if(Component.getInitialProps) {
             pageProps = await Component.getInitialProps(ctx);
